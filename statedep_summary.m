ea668c8d888227ec895b19af33bd1ccde9ec8e02
ecarmichael <ecarmichael@gmail.com>
2019-01-27 03:20:52 -0700

    simplified version of the phase_latency sandbox.  new summary figures and color PETH

--- statedep_summary.m
+++ statedep_summary.m
@@ -83,17 +83,27 @@ for iC = 1:length(cell_list)
         if strcmp(f_list{iF}, 'hdr')
             continue
         else
+                    x_phase_resp(iC,iF) = nanmean(all_cells.(this_cell).(f_list{iF}).resp(2,:));
+
             for iPhase = unique(all_cells.(this_cell).(f_list{iF}).latency(1,:)) % get the phase segment numbers (should be 1:5 if phase split by 5
+               % labels for x axes
+               phase_labels{iPhase} = ['p' num2str(iPhase)];
+
                 % get the latencies
                 this_phase_idx = find(all_cells.(this_cell).(f_list{iF}).latency(1,:) == iPhase);
                 all_lat(iC, iPhase,iF)  = nanmean(all_cells.(this_cell).(f_list{iF}).latency(2,this_phase_idx)); % in ms get the mean value for this cell in this freq at this phase bin
-                
                 % same for spike count
                 all_count(iC, iPhase, iF) = nanmean(all_cells.(this_cell).(f_list{iF}).count(2,this_phase_idx)); % in ms get the mean value for this cell in this freq at this phase bin
                 
+                all_resp(iC,iPhase,iF) = nanmean(all_cells.(this_cell).(f_list{iF}).resp(2,this_phase_idx));
+                
+                % get all the reponses
                 % maybe shuffles?
-                phase_labels{iPhase} = ['p' num2str(iPhase)];
+%                 all_count_shuf(iC, iPhase, iF) = nanmean(all_cells.(this_cell).(f_list{iF}).	(2,this_phase_idx)); % in ms get the mean value for this cell in this freq at this phase bin
+
             end
+            z_lat(iC, :,iF) = zscore(all_lat(iC, :,iF));
+
             
         end
     end
@@ -123,18 +133,18 @@ set(gcf, 'position', [282   50  1200  720])
 ha = axes('Position',[0 0 1 1],'Xlim',[0 1],'Ylim',[0  1],'Box','off','Visible','off','Units','normalized', 'clipping' , 'off');
 text(0.35, 0.98,['Latency to first spike across cells'], 'fontsize', font_size)
 
-saveas(gcf, [summary_dir 'Summary_latency.png']);
-saveas_eps('Summary_latency.png', summary_dir)
+% saveas(gcf, [summary_dir 'Summary_latency_raw.png']);
+% saveas_eps('Summary_latency_raw.png', summary_dir)
 
-
-% same but for counts
+%%
+% same but for zscore
 figure(2)
 for iF = 1:length(f_list)
     if strcmp(f_list{iF}, 'hdr')
         continue
     else
         subplot(2,ceil(length(f_list)-1)/2, iF)
-        imagesc(all_count(:,:,iF))
+        imagesc(z_lat(:,:,iF))
         axis xy
         xlabel('phase')
         ylabel('cell id')
@@ -142,11 +152,50 @@ for iF = 1:length(f_list)
         text(floor(length(phase_labels)/2),length(all_lat)+1, f_list{iF}, 'fontsize', font_size)
         
         colorbar
+        caxis([-2.5 2.5])
     end
 end
 SetFigure([], gcf)
 set(gcf, 'position', [282   50  1200  720])
 ha = axes('Position',[0 0 1 1],'Xlim',[0 1],'Ylim',[0  1],'Box','off','Visible','off','Units','normalized', 'clipping' , 'off');
-text(0.35, 0.98,['Light evoked spike count across cells'], 'fontsize', font_size)
-saveas(gcf, [summary_dir 'Summary_count.png']);
-saveas_eps('Summary_count.png', summary_dir)
+text(0.35, 0.98,['Zscore first spike latency across cells'], 'fontsize', font_size)
+% saveas(gcf, [summary_dir 'Summary_count.png']);
+% saveas_eps('Summary_count.png', summary_dir)
+
+
+%% make a response plot
+
+
+
+% get all responses p(Spike|stim)
+
+% x_phase_resp should be the same across f_list since it is the number of
+% responses and doesn't care about phase or freq. 
+
+for iC =1:size(all_resp,1)
+    for iF = 1:size(all_resp,3)
+% get max phase
+[max_val, max_idx] = max(all_resp(iC,:,iF));
+
+% get min phase
+[min_val, min_idx] = min(all_resp(iC,:,iF));
+
+
+% get ratio
+resp_ratio(iC,iF) = max_val/min_val; 
+
+    end
+end
+resp_ratio_1d = reshape(resp_ratio, 1, numel(resp_ratio));
+resp_all_1d = reshape(x_phase_resp, 1, numel(x_phase_resp));
+
+
+
+% generate figure
+figure(3)
+% subplot(2,1,1)
+scatter(resp_all_1d, resp_ratio_1d,100, 'filled')
+xlabel('p(spike|stim)')
+ylabel('ratio of max phase / min phase response')
+SetFigure([], gcf)
+
